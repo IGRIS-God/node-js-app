71c2ec4b76c7fb716392004bc33d1440e18718fd
Sandesh Neupane <sandeshpkr977@gmail.com>
2023-11-03 13:58:24 +0545

    initial commit

---
+++ app.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const app = express();
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+
+const productRoutes = require("./api/routes/products");
+const orderRoutes = require("./api/routes/orders");
+
+app.use(morgan("dev")); //using before routes to send request only after passing it through morgan
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*"); // to allow other domain to use my api , to dont allow them write own domain name only , http;//.......
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  if (req.method === "OPTIONS") {
+    res.header(
+      "Access-Control-Allow-Methods",
+      "PUT",
+      "POST",
+      "PATCH",
+      "DELETE",
+      "GET"
+    );
+    return res.status(200), json({});
+  }
+  next();
+});
+
+// Routes which should handle requests
+app.use("/products", productRoutes);
+app.use("/orders", orderRoutes);
+
+app.use((req, res, next) => {
+  const error = new Error("Not Found");
+  error.status = 404;
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message,
+    },
+  });
+});
+
+module.exports = app;
